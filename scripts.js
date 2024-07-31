f7f06a9ef69a5509a884d3ad16cf7d13da13eaa1
Ryanlucas2468 <ryanlucas24681012@gmail.com>
2024-07-31 03:19:52 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Define a imagem de fundo inicial
+    document.body.style.backgroundImage = "url('imagens/inteleon.jpg')";
+
+    document.getElementById('form').addEventListener('submit', function(event) {
+        event.preventDefault(); // Evita o envio do formulário
+
+        // Obter o valor do campo de senha
+        const password = document.getElementById('password').value;
+
+        // Verificar se a senha está correta
+        if (password === "Aphonsy Ossinho Santos de Azevedo Gutemberg") {
+            // Ocultar o formulário
+            document.getElementById('access-form').style.display = 'none';
+
+            // Exibir o conteúdo principal
+            document.getElementById('main-content').style.display = 'block';
+
+            // Alterar a imagem de fundo
+            document.body.style.backgroundImage = "url('imagens/fundo.jpg')"; // Substitua pelo caminho da nova imagem de fundo
+        } else {
+            alert('Senha incorreta. Tente novamente.');
+        }
+    });
+});
